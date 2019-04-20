207211aa56f686d4494266a4e44f826e3727e867
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2019-04-19 17:07:56 -0700

    Update better-error-messages.php

--- better-error-messages.php
+++ better-error-messages.php
@@ -10,14 +10,3 @@
 
 ini_set('error_prepend_string','<div class="notice notice-error is-dismissible">')  ;
 ini_set('error_append_string',"</div>")  ;
-
-//ini_set('warning_prepend_string','<div class="notice notice-error is-dismissible">')  ;
-//ini_set('warning_append_string',"</div>")  ;
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-//x = 4;
-//$y = $x / 0;
-//$c = create_function();
-//$c = create_function(1,2);
-//Deprecated: Function create_function() is deprecated in C:\xampp\htdocs\wp-content\plugins\asteroids-widget\asteroids-widget-by-electric-tree-house.php on line 351
